25473f85048503c7bcf5488aaf7715feb923310f
Marlon Vera <marlon.vera@ias.com.co>
2024-02-27 15:53:29 -0500

    Added routes to all components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
-export class DashboardComponent {
+export default class DashboardComponent {
 
 }
